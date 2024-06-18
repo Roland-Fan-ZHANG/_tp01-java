fb14911fc96c68f3f62f2da1acba3bd11e3e5e7e
Roland <76219145+Roland-Fan-ZHANG@users.noreply.github.com>
2024-06-18 22:48:40 +0200

    exercice 2

--- src/_tp01/PrintArgs.java
+++ src/_tp01/PrintArgs.java
@@ -2,8 +2,19 @@ package _tp01;
 
 public class PrintArgs {
 	public static void main(String[] args) {
+		/* Exercice 2.1
+		System.out.println(args[0]);
+		*/
+		
+		/* Exercice 2.2
 		for(int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
+		}
+		*/
+		
+		// Exercice 2.3
+		for(var element : args) {
+			System.out.println(element);
 		}
 	}
 }
